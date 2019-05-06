6c2ccfbfd0d42a927727864b6b21ca71eb092af3
slwu89 <slwu89@berkeley.edu>
2019-05-06 16:32:33 -0700

    add foreach example

---
+++ standalone/foreach/foreach.R
@@ -0,0 +1,24 @@
+Rcpp::sourceCpp(here::here('foreach/timestwo.cpp'))
+
+timesTwo(5)
+
+library(parallel)
+library(foreach)
+library(doSNOW)
+
+cl <- makeSOCKcluster(2)
+registerDoSNOW(cl)
+
+# rm(timesTwo)
+
+# clusterEvalQ(cl,{
+#   Rcpp::sourceCpp('Desktop/code/timestwo.cpp')
+# })
+
+
+foreach(i = 1:10,.combine = "c") %dopar% {
+  timesTwo(i)
+}
+
+stopCluster(cl)
+rm(cl);gc()
---
+++ standalone/foreach/timestwo.cpp
@@ -0,0 +1,17 @@
+#include <Rcpp.h>
+
+// This is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp 
+// function (or via the Source button on the editor toolbar). Learn
+// more about Rcpp at:
+//
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//   http://gallery.rcpp.org/
+//
+
+// [[Rcpp::export]]
+Rcpp::NumericVector timesTwo(Rcpp::NumericVector x) {
+  return x * 2;
+}
+
